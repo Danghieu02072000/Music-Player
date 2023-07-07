6f393b59c2dea465b2444cf1124692856a10e8ba
Dang Hieu <danghieutieumai@gmail.com>
2023-07-07 17:29:08 +0700

    add js scoll-top

--- main.js
+++ main.js
@@ -29,7 +29,19 @@ const app = {
                     singer:'Avicii',
                     path: './assets/mp3/wake_me_up.mp3',
                     image:'./assets/images/up.jpg'
-                }
+                },
+                {
+                    name: 'Wake Me Up',
+                    singer:'Avicii',
+                    path: './assets/mp3/wake_me_up.mp3',
+                    image:'./assets/images/up.jpg'
+                },
+                {
+                    name: 'Wake Me Up',
+                    singer:'Avicii',
+                    path: './assets/mp3/wake_me_up.mp3',
+                    image:'./assets/images/up.jpg'
+                },
             ],
     render: function() {
         var listSongs = document.querySelector('.music__playlist');
@@ -46,8 +58,21 @@ const app = {
         html = html.join('');
         listSongs.innerHTML = html;
     },
+    handleEvents: function() {
+        var imgCd = document.querySelector('.music__cd-img');
+        var widthImgCd = imgCd.offsetHeight
+        document.onscroll = function() {
+            var scollY = window.scrollY || document.documentElement.scrollTop;
+  
+            var newWidthImgCd = widthImgCd - scollY  ;
+            imgCd.style.width = newWidthImgCd > 0 ? newWidthImgCd + 'px': 0 ;
+            imgCd.style.height = newWidthImgCd > 0 ? newWidthImgCd +'px ': 0 ;
+            imgCd.style.opacity = newWidthImgCd / widthImgCd;
+        }
+    },
     start : function() {
         this.render()
+        this.handleEvents()
     }
 
 
